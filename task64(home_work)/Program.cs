1035545350168f22463602487d05cd84c8eaee00
Skich <skich.v@gmail.com>
2023-10-22 19:14:28 +0500

    dz_8sem

---
+++ task64(home_work)/Program.cs
@@ -0,0 +1,10 @@
+﻿void RoadOfN(int number)
+{
+for (int i = number; i >= 1; i--) 
+{ 
+Console.Write(i + " "); 
+} 
+}
+System.Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+RoadOfN(number);
---
+++ task68(home_work)/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return akkerman(m - 1, 1);
+    else return akkerman(m - 1, akkerman(m, n - 1));
+}
+Console.Write($"Функция Аккермана равна {akkerman(m,n)}");
